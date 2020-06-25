57bb8d207dcd58db0dfdcde4ae597bd508c07054
SeanRog <seanmikerog@gmail.com>
2020-06-24 22:10:43 -0700

    Added models, views, and controllers for Category, Company, CoverType, and Product.

---
+++ BulkyBook.DataAccess/Repository/SP_Call.cs
@@ -0,0 +1,93 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class SP_Call : ISP_Call
+    {
+
+        private readonly ApplicationDbContext _db;
+        private static string ConnectionString = "";
+
+        public SP_Call(ApplicationDbContext db)
+        {
+            _db = db;
+            ConnectionString = db.Database.GetDbConnection().ConnectionString;
+        }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        //if you have a stored procedure call that should update, use this method
+        public void Execute(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+            {
+                sqlCon.Open();
+                sqlCon.Execute(procedureName, param, commandType: System.Data.CommandType.StoredProcedure);
+            }
+        }
+
+
+        //if you have a stored procedure that should retrieve all categories, use this method
+        public IEnumerable<T> List<T>(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+            {
+                sqlCon.Open();
+                return sqlCon.Query<T>(procedureName, param, commandType: System.Data.CommandType.StoredProcedure);
+            }
+        }
+
+        //if you have a stored procedure that should return two tables, use this method (can also be used to return 3 tables)
+        public Tuple<IEnumerable<T1>, IEnumerable<T2>> List<T1, T2>(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+            {
+                sqlCon.Open();
+                var result = SqlMapper.QueryMultiple(sqlCon, procedureName, param, commandType: System.Data.CommandType.StoredProcedure);
+                var item1 = result.Read<T1>().ToList();
+                var item2 = result.Read<T2>().ToList();
+                
+
+                if(item1 != null && item2 != null)
+                {
+                    return new Tuple<IEnumerable<T1>, IEnumerable<T2>>(item1, item2);
+                }
+            }
+
+            return new Tuple<IEnumerable<T1>, IEnumerable<T2>>(new List<T1>(), new List<T2>());
+        }
+
+        //retrieves one record (or one row)
+        public T OneRecord<T>(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+            {
+                sqlCon.Open();
+                var value = sqlCon.Query<T>(procedureName, param, commandType: System.Data.CommandType.StoredProcedure);
+                return (T)Convert.ChangeType(value.FirstOrDefault(), typeof(T));
+            }
+        }
+
+
+        public T Single<T>(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+            {
+                sqlCon.Open();
+                return (T)Convert.ChangeType(sqlCon.ExecuteScalar<T>(procedureName, param, commandType: System.Data.CommandType.StoredProcedure), typeof(T));
+            }
+        }
+    }
+}
---
+++ BulkyBook/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CompanyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CompanyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            Company company;
+            
+            if(id == null)
+            {
+                //create
+                company = new Company();
+                return View(company);
+            }
+            //edit
+            company = _unitOfWork.Company.Get(id.GetValueOrDefault());
+            if(company == null)
+            {
+                return NotFound();
+            }
+            return View(company);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Company company)
+        {
+
+            if (ModelState.IsValid)
+            {
+                if(company.Id == 0)
+                {
+                    //create
+                    _unitOfWork.Company.Add(company);
+                }
+                else
+                {
+                    //edit
+                    _unitOfWork.Company.Update(company);
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                if(company.Id != 0)
+                {
+                    company = _unitOfWork.Company.Get(company.Id);
+                }
+                return View(company);
+            }
+
+            Debug.WriteLine("ModelState not valid");
+            return View(company);
+        }
+
+
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var objFromDb = _unitOfWork.Company.GetAll();
+            return Json(new { data = objFromDb });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.Company.Get(id);
+            if(objFromDb == null)
+            {
+                return Json(new { success = "false", message = "Error while deleting" });
+            }
+            _unitOfWork.Company.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = "true", message = "Delete Successful" });
+        }
+
+        #endregion
+
+
+    }
+}
